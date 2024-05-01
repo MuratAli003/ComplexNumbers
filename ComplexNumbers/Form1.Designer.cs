02ad331f09860da098b4ac7dd3d3841e889b55cf
huawei <huawei@LAPTOP-2R5A6JR2>
2024-05-02 00:59:28 +0300

    Proje dosyası ekle.

---
+++ ComplexNumbers/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ComplexNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gercek1 = new System.Windows.Forms.TextBox();
+            this.sanal1 = new System.Windows.Forms.TextBox();
+            this.gercek2 = new System.Windows.Forms.TextBox();
+            this.sanal2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.sonuc_alan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(52, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(165, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gerçek";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(323, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sanal";
+            // 
+            // gercek1
+            // 
+            this.gercek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gercek1.Location = new System.Drawing.Point(151, 69);
+            this.gercek1.Multiline = true;
+            this.gercek1.Name = "gercek1";
+            this.gercek1.Size = new System.Drawing.Size(100, 34);
+            this.gercek1.TabIndex = 4;
+            // 
+            // sanal1
+            // 
+            this.sanal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sanal1.Location = new System.Drawing.Point(302, 69);
+            this.sanal1.Multiline = true;
+            this.sanal1.Name = "sanal1";
+            this.sanal1.Size = new System.Drawing.Size(100, 34);
+            this.sanal1.TabIndex = 5;
+            // 
+            // gercek2
+            // 
+            this.gercek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gercek2.Location = new System.Drawing.Point(151, 123);
+            this.gercek2.Multiline = true;
+            this.gercek2.Name = "gercek2";
+            this.gercek2.Size = new System.Drawing.Size(100, 34);
+            this.gercek2.TabIndex = 6;
+            // 
+            // sanal2
+            // 
+            this.sanal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sanal2.Location = new System.Drawing.Point(302, 123);
+            this.sanal2.Multiline = true;
+            this.sanal2.Name = "sanal2";
+            this.sanal2.Size = new System.Drawing.Size(100, 34);
+            this.sanal2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(444, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(525, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(444, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(525, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(408, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "i";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(408, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "i";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(358, 212);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Sonuç:";
+            // 
+            // sonuc_alan
+            // 
+            this.sonuc_alan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sonuc_alan.Location = new System.Drawing.Point(444, 209);
+            this.sonuc_alan.Multiline = true;
+            this.sonuc_alan.Name = "sonuc_alan";
+            this.sonuc_alan.Size = new System.Drawing.Size(115, 29);
+            this.sonuc_alan.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 360);
+            this.Controls.Add(this.sonuc_alan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sanal2);
+            this.Controls.Add(this.gercek2);
+            this.Controls.Add(this.sanal1);
+            this.Controls.Add(this.gercek1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ComplexNumbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gercek1;
+        private System.Windows.Forms.TextBox sanal1;
+        private System.Windows.Forms.TextBox gercek2;
+        private System.Windows.Forms.TextBox sanal2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox sonuc_alan;
+    }
+}
+
---
+++ ComplexNumbers/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComplexNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            complex obj = new complex();
+
+            if (obj.sayiKontrol(gercek1.Text) || obj.sayiKontrol(sanal1.Text) || obj.sayiKontrol(sanal2.Text) || obj.sayiKontrol(gercek2.Text))
+            {
+                MessageBox.Show("Sayı dışında değer girmeyiniz!");
+            }
+            else
+            {
+                if (gercek1.Text != "" && sanal1.Text != "" && sanal2.Text != "" && gercek2.Text != "")
+                {
+                    double aGercek = Convert.ToDouble(gercek1.Text);
+                    double aSanal = Convert.ToDouble(sanal1.Text);
+
+                    double bGercek = Convert.ToDouble(gercek2.Text);
+                    double bSanal = Convert.ToDouble(sanal2.Text);
+
+                    complex num1 = new complex(aGercek, aSanal);
+                    complex num2 = new complex(bGercek, bSanal);
+
+                    complex sonuc = num1 + num2;
+
+                    
+                    sonuc_alan.Text = sonuc.yaz(sonuc);
+                    gercek1.Text = sanal1.Text = sanal2.Text = gercek2.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Sayı kutularını doldurunuz!");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            complex obj = new complex();
+
+            if (obj.sayiKontrol(gercek1.Text) || obj.sayiKontrol(sanal1.Text) || obj.sayiKontrol(sanal2.Text) || obj.sayiKontrol(gercek2.Text))
+            {
+                MessageBox.Show("Sayı dışında değer girmeyiniz!");
+            }
+            else
+            {
+                if (gercek1.Text != "" && sanal1.Text != "" && sanal2.Text != "" && gercek2.Text != "")
+                {
+                    double aGercek = Convert.ToDouble(gercek1.Text);
+                    double aSanal = Convert.ToDouble(sanal1.Text);
+
+                    double bGercek = Convert.ToDouble(gercek2.Text);
+                    double bSanal = Convert.ToDouble(sanal2.Text);
+
+                    complex num1 = new complex(aGercek, aSanal);
+                    complex num2 = new complex(bGercek, bSanal);
+
+                    complex sonuc = num1 - num2;
+
+                    sonuc_alan.Text = sonuc.yaz(sonuc);
+                    gercek1.Text = sanal1.Text = sanal2.Text = gercek2.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Sayı kutularını doldurunuz!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            complex obj = new complex();
+
+            if (obj.sayiKontrol(gercek1.Text) || obj.sayiKontrol(sanal1.Text) || obj.sayiKontrol(sanal2.Text) || obj.sayiKontrol(gercek2.Text))
+            {
+                MessageBox.Show("Sayı dışında değer girmeyiniz!");
+            }
+            else
+            {
+                if (gercek1.Text != "" && sanal1.Text != "" && sanal2.Text != "" && gercek2.Text != "")
+                {
+                    double aGercek = Convert.ToDouble(gercek1.Text);
+                    double aSanal = Convert.ToDouble(sanal1.Text);
+
+                    double bGercek = Convert.ToDouble(gercek2.Text);
+                    double bSanal = Convert.ToDouble(sanal2.Text);
+
+                    complex num1 = new complex(aGercek, aSanal);
+                    complex num2 = new complex(bGercek, bSanal);
+
+                    complex sonuc = num1 * num2;
+
+                    sonuc_alan.Text = sonuc.yaz(sonuc);
+                    gercek1.Text = sanal1.Text = sanal2.Text = gercek2.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Sayı kutularını doldurunuz!");
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            complex obj = new complex();
+
+            if (obj.sayiKontrol(gercek1.Text) || obj.sayiKontrol(sanal1.Text) || obj.sayiKontrol(sanal2.Text) || obj.sayiKontrol(gercek2.Text))
+            {
+                MessageBox.Show("Sayı dışında değer girmeyiniz!");
+            }
+            else
+            {
+                if (gercek1.Text != "" && sanal1.Text != "" && sanal2.Text != "" && gercek2.Text != "")
+                {
+                    double aGercek = Convert.ToDouble(gercek1.Text);
+                    double aSanal = Convert.ToDouble(sanal1.Text);
+
+                    double bGercek = Convert.ToDouble(gercek2.Text);
+                    double bSanal = Convert.ToDouble(sanal2.Text);
+
+                    complex num1 = new complex(aGercek, aSanal);
+                    complex num2 = new complex(bGercek, bSanal);
+
+                    complex sonuc = num1 / num2;
+
+                    sonuc_alan.Text = sonuc.yaz(sonuc);
+                    gercek1.Text = sanal1.Text = sanal2.Text = gercek2.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Sayı kutularını doldurunuz!");
+                }
+            }
+
+        }
+    }
+    public class complex
+    {
+        private double nGercek;
+        private double nSanal;
+
+        public complex(double gercek, double sanal)
+        {
+            this.nGercek = gercek;
+            this.nSanal = sanal;
+        }
+
+        public complex()
+        {
+            nGercek = 0;
+            nSanal = 0;
+        }
+        public complex(complex x)
+        {
+            x.nSanal = this.nSanal;
+            x.nGercek = this.nGercek;
+        }
+        public double gercek
+        {
+            get { return this.nGercek; }
+            set { this.nGercek = value; }
+        }
+        public double sanal
+        {
+            get { return this.nSanal; }
+            set { this.nSanal = value; }
+        }
+
+        public String yaz(complex x)
+        {
+            if (x.nSanal >= 0)
+            {
+                if (x.nGercek == 0 && x.nSanal == 0)
+                {
+                    return "0";
+                }
+
+                else if (x.nGercek != 0 && x.nSanal == 0)
+                {
+                    string gercek = Convert.ToString(x.nGercek);
+                    return gercek;
+                }
+
+                else if (x.nGercek == 0 && x.nSanal != 0)
+                {
+                    if (x.nSanal == 1)
+                    {
+                        return "+i";
+                    }
+                    else if (x.nSanal == -1)
+                    {
+                        return "-i";
+                    }
+                    else
+                    {
+                        string sanal = Convert.ToString(x.nSanal);
+                        return sanal + "i";
+                    }
+                }
+
+                else
+                {
+                    if (x.nSanal == 1)
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        return gercek + "+i";
+                    }
+                    else if (x.nSanal == -1)
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        return gercek + "-i";
+                    }
+                    else
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        string sanal = Convert.ToString(x.nSanal);
+                        return gercek + "+" + sanal + "i";
+                    }
+                }
+            }
+
+            else
+            {
+                if (x.nGercek == 0 && x.nSanal == 0)
+                {
+                    return "0";
+                }
+
+                else if (x.nGercek != 0 && x.nSanal == 0)
+                {
+                    string gercek = Convert.ToString(x.nGercek);
+                    return gercek;
+                }
+
+                else if (x.nGercek == 0 && x.nSanal != 0)
+                {
+                    if (x.nSanal == 1)
+                    {
+                        return "+i";
+                    }
+                    else if (x.nSanal == -1)
+                    {
+                        return "-i";
+                    }
+                    else
+                    {
+                        string sanal = Convert.ToString(x.nSanal);
+                        return sanal + "i";
+                    }
+                }
+                else
+                {
+                    if (x.nSanal == 1)
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        return gercek + "+i";
+                    }
+                    else if (x.nSanal == -1)
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        return gercek + "-i";
+                    }
+                    else
+                    {
+                        string gercek = Convert.ToString(x.nGercek);
+                        string sanal = Convert.ToString(x.nSanal);
+                        return gercek + sanal + "i";
+                    }
+                }
+            }
+
+        }
+        public Boolean sayiKontrol(string x)
+        {
+            string kontrol = "qwertyuıopğüasdfghjklşizxcvbnmöçQWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇ.,<>*/";
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < kontrol.Length; j++)
+                {
+                    if (x[i] == kontrol[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static complex operator +(complex x, complex y)
+        {
+            double aGercek = x.nGercek + y.nGercek;
+            double aSanal = x.sanal + y.sanal;
+
+            return new complex(aGercek, aSanal);
+
+        }
+        public static complex operator -(complex x, complex y)
+        {
+            double aGercek = x.nGercek - y.nGercek;
+            double aSanal = x.sanal - y.sanal;
+
+            return new complex(aGercek, aSanal);
+
+        }
+        public static complex operator *(complex x, complex y)
+        {
+            double aSanal = (x.nSanal * y.nGercek) + (x.nGercek * y.nSanal);
+            double aGercek = (x.nGercek * y.nGercek) + ((-1) * (x.nSanal * y.sanal));
+
+            return new complex(aGercek, aSanal);
+        }
+
+        public static complex operator /(complex x, complex y)
+        {
+            complex y1 = new complex(y.nGercek, -1 * y.nSanal);
+
+            complex pay = x * y1;
+            complex payda = y * y1;
+
+            double aSanal = pay.nSanal / payda.nGercek;
+            double aGercek = pay.nGercek / payda.nGercek;
+
+            return new complex(aGercek, aSanal);
+        }
+    }
+}
